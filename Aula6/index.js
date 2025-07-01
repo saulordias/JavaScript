4f01c09cb6bef66bf0b70b949957d6d7550c187b
Saulo Dias <saulo.dias@viajarbarato.com.br>
2025-07-01 17:49:16 -0300

    Nova atualização

---
+++ Aula6/index.js
@@ -0,0 +1,37 @@
+/*let nome = 'João'; //Existe como declarar como var, mas é algo antigo.
+
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve um filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');*/
+
+
+/*let nome; // declarou a variável, mas não inicializou
+nome= 'João'; // inicializou a variável
+console.log(nome);
+nome = 'Otávio'; // reatribuiu a variável
+console.log(nome);
+
+//Não podemos declarar variáveis com palavras reservadas, como let, var, const, if, else, etc.
+//Variáveis precisam ter nomes significativos.
+
+//let n = 'João'; // Exemplo de nome não significativo, dificultando a compreensão do código.*/
+
+let nomeCliente = 'João';
+console.log(nomeCliente);
+
+// Não pode começar o nome da variável com número
+// let 1nomeCliente = 'João'; // Isso é inválido
+// Não pode conter espaços ou caracteres especiais
+// let nome Cliente = 'João'; // Isso é inválido
+// Case sensitive: JavaScript diferencia maiúsculas de minúsculas
+//utilizamos camelCase para nomear variáveis
+// Não podemos redeclarar uma variáveis com let
+// Não utilize var, utilize let.
+
+let nomeCompletoDoCliente = 'João da Silva';
+console.log(nomeCompletoDoCliente);
+
+
