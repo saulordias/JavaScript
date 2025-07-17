9d525ada078275ccad79737a653a2b7c9350dc65
Saulo Dias <saulo.dias@viajarbarato.com.br>
2025-07-17 18:14:18 -0300

    Removendo arquivos de exemplos de operadores lógicos, comparação e estruturas condicionais, e atualizando a documentação do curso e os arquivos de estilo e script para o novo modelo de cálculo de IMC.

---
+++ 2 - JavaScript - Lógica de programação/Aula27 - Operação ternaria/index.js
@@ -0,0 +1,22 @@
+// ? : 
+
+
+// Exemplo 
+
+/*
+const pontuacaoUsuario = 999;
+
+if (pontuacaoUsuario >= 1000) console.log('usuario VIP');
+else console.log('usuário normal');
+
+*/
+
+////////////////// EXEMPLO FAZENDO DIRETO a operação ternaria
+
+
+// (condição) ? 'VALOR VERDADEIRO' : 'VALOR PARA FALSO'
+
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+
+console.log(nivelUsuario);
--- Modelo/assets/js/main.js
+++ Modelo/assets/js/main.js
@@ -1,1 +1,39 @@
-alert (1);
+
+function meuEscopo() {
+
+    const form = document.querySelector('.forma');
+    const resultado = document.querySelector('.resultado');
+
+
+
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+
+        const peso = Number(form.querySelector('input[name="peso"]').value);
+        const altura = Number(form.querySelector('input[name="altura"]').value);
+
+
+        const imc = peso / (altura * altura);
+        
+        console.log(imc.toFixed(2));
+
+
+        if (imc <= 18.5) {
+
+
+
+        }
+
+    };
+
+
+
+
+    form.addEventListener('submit', recebeEventoForm);
+
+};
+
+
+meuEscopo();
+
+
