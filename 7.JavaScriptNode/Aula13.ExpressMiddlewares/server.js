615bf231fea112f5e1ba4683583939627b3f0b4d
Saulo Dias <saulo.dias@viajarbarato.com.br>
2025-08-12 14:15:39 -0300

    Salvando novas aulas

---
+++ 7.JavaScriptNode/Aula13.ExpressMiddlewares/server.js
@@ -0,0 +1,37 @@
+// Como utilizar Router e já fazer as aplicações de controller
+
+const express = require('express');
+
+const app = express();
+
+const routes = require('./routes'); // Importando as rotas
+
+const path = require('path');
+
+const {middlewareGlobal} = require('./src/middlewares/middlewares');
+
+
+
+
+app.use(express.urlencoded({ extended: true })); // Aqui estamos dizendo para o Express interpretar o body do formulário (req.body)
+
+app.use(express.static(path.resolve(__dirname, 'public'))); // Definindo a pasta pública para arquivos estáticos (CSS, JS, imagens, etc.)
+
+app.set('views', path.resolve(__dirname, 'src', 'views')); // Definindo onde estão as views
+app.set('view engine', 'ejs'); // Definindo a engine de template que vamos usar (EJS)
+
+
+// Nossos próprios Middlewares
+app.use(middlewareGlobal); // Usando o middleware global
+
+
+app.use(routes); // Usando as rotas importadas
+
+
+
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor rodando na porta 3000');
+
+}); // O servidor irá escutar na porta 3000 
---
+++ 7.JavaScriptNode/Aula14.MongoDB/src/controllers/homeController.js
@@ -0,0 +1,15 @@
+exports.paginaInicial = (req, res) => {
+    res.render('index');
+    return;
+
+
+
+};
+
+// 
+exports.trataPost = (req, res) => {
+    res.send(req.body);
+    return;
+};
+
+
